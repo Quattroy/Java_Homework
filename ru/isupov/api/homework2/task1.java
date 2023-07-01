aaa8afd24f8603565fb83c78f1ce130f3758f3b9
Igor Isupov <igor-i.s1@ya.ru>
2023-07-02 00:49:53 +0300

    Задча №1 Семинар 2

---
+++ ru/isupov/api/homework2/task1.java
@@ -0,0 +1,29 @@
+// Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, 
+//используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+package ru.isupov.api.homework2;
+import java.util.HashMap;
+import java.util.Map;
+
+public class task1 {
+    public static void main(String[] args) {
+        Map<String, String> parametr = new HashMap<String, String>();
+        parametr.put("name", "Ivanov");
+        parametr.put("country", "Russia");
+        parametr.put("city", "Moscow");
+        parametr.put("age", "null");
+        System.out.println(query(parametr));
+    }
+    public static String query(Map<String, String> parametr) {
+        StringBuilder str = new StringBuilder();
+        for(Map.Entry<String, String> item : parametr.entrySet()){
+            if(item.getValue() != null) {
+                str.append(item.getKey() + " = " + item.getValue() + " and ");
+            }
+        }
+        return str.toString();
+    }
+}
+
